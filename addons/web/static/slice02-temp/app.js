f8c9946168e254bd8647f8f0c271330c331a243d
Mohamed Azzain <m.alzain248@gmail.com>
2022-06-01 09:47:27 +0200

    Modified the Components Architecture
    Added WebClient as a root component, it includes NavBar and
    ActionManager. The ActionManager serves as a place for components
    switching.
    With regard to the login process, for now it's a fake one, we just allow
    any user who filled the fields and submit, to login. After the login,
    Course will be the component to be displayed.

--- addons/web/static/slice02-temp/app.js
+++ addons/web/static/slice02-temp/app.js
@@ -1,10 +1,28 @@
 
-const { useState, useRef } = owl;
+const { Component, xml, EventBus, onWillDestroy, useState, useRef } = owl;
+
+function makeEnv() {
+    return {
+        bus: new EventBus()
+    };
+}
 
 class VisitorList extends owl.Component {}
 VisitorList.template = 'VisitorList'
 
-class SignIn extends owl.Component {}
+class SignIn extends owl.Component {
+
+    setup(){
+        this.onLogIn = this.onLogIn.bind(this);
+    }
+
+    onLogIn(event){
+        event.stopPropagation();
+        event.preventDefault();
+        console.log('Log in info: ', { userName: 'John', password: 'pass' });
+        this.env.bus.trigger("LOGIN_BUTTON:CLICKED", { userName: 'John', password: 'pass' });
+    }
+}
 SignIn.template = "SignIn"
 
 class Course extends owl.Component {}
@@ -22,10 +40,62 @@ Service.template = "Service"
 class About extends owl.Component {}
 About.template = "About"
 
+class NavBar extends Component {
+    setup(){
+        this.onClick = this.onClick.bind(this);
+    }
+    onClick(selectComponent){
+        console.log('The clicked item: ', selectComponent);
+        this.env.bus.trigger("ACTION_MANAGER:UPDATE", { selectComponent });
+    }
+}
+NavBar.template = "NavBar"
 
-class ActionManager extends owl.Component {}
-ActionManager.template = "ActionManager"
+class ActionManager extends owl.Component {
+    setup() {
+        this.info = { Component: Service };
+        this.onActionManagerUpdate = ({ detail }) => {
+            this.info = { Component: ActionManager.components[detail.selectComponent] };
+            this.render();
+        };
+        this.env.bus.addEventListener("ACTION_MANAGER:UPDATE", this.onActionManagerUpdate);
+        onWillDestroy(() => {
+            this.env.bus.removeEventListener("ACTION_MANAGER:UPDATE", this.onActionManagerUpdate);
+        });
+    }
+}
+ActionManager.template = xml`
+    <t t-name="ActionManager">
+        <div>
+            <t t-if="info.Component" t-component="info.Component" t-props="info.componentProps"/>
+        </div>
+    </t>`;
+
 ActionManager.components = { Service, SignIn, Team, About, Contact, Course }
+
+class WebClient extends Component {
+
+    render(args){
+        super.render(args);
+    }
+
+    setup(){
+        this.state = useState({ user: { loggedIn: false } });
+        this.onLogIn = this.onLogIn.bind(this);
+        this.env.bus.addEventListener("LOGIN_BUTTON:CLICKED", this.onLogIn);
+    }
+
+    onLogIn(){
+        this.state.user = { loggedIn: true };
+        this.render();
+        this.env.bus.trigger("ACTION_MANAGER:UPDATE", { selectComponent: 'Course' });
+    }
+}
+WebClient.components = {
+    ActionManager,
+    NavBar,
+};
+WebClient.template = "web.WebClient";
 
 
 class VisitorForm extends owl.Component {
@@ -106,7 +176,8 @@ async function start() {
     owl.loadFile("web/static/slice02-temp/templates.xml"),
     owl.whenReady()
   ]);
-  const rootApp = new owl.App(ActionManager);
+  const env = makeEnv();
+  const rootApp = new owl.App(WebClient, { env });
   rootApp.addTemplates(templates);
 
   await  rootApp.mount(document.getElementById('action_manager'));
