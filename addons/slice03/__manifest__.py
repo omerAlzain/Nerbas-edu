fcdbf7629f2c23c4e7eff0d31487709ca502938b
Mohamed Azzain <m.alzain248@gmail.com>
2022-06-06 12:23:14 +0200

    Remove The Stratbootstrap Dependencies
    Stratbootstrap is the one who creates boostrap themes. Slice03 theme
    happend to be created by them. The theme has dependency on bootstrap
    library, and it is usually bundled together. And since odoo itself has a
    bootstrap library bundled in the web module, we decided to delete the
    one that come with the theme.

--- addons/slice03/__manifest__.py
+++ addons/slice03/__manifest__.py
@@ -26,11 +26,28 @@ This module is to practice single page application techniques.
             'web/static/lib/jquery/jquery.js',
             'web/static/lib/bootstrap/css/bootstrap.css',
             'web/static/src/legacy/js/promise_extension.js',
+
+            'web/static/lib/bootstrap/js/index.js',
+            'web/static/lib/bootstrap/js/util.js',
+            'web/static/lib/bootstrap/js/alert.js',
+            'web/static/lib/bootstrap/js/button.js',
+            'web/static/lib/bootstrap/js/carousel.js',
+            'web/static/lib/bootstrap/js/collapse.js',
+            'web/static/lib/bootstrap/js/dropdown.js',
+            'web/static/lib/bootstrap/js/modal.js',
+            'web/static/lib/bootstrap/js/tooltip.js',
+            'web/static/lib/bootstrap/js/popover.js',
+            'web/static/lib/bootstrap/js/scrollspy.js',
+            'web/static/lib/bootstrap/js/tab.js',
+            'web/static/lib/bootstrap/js/toast.js',
+
             'web/static/src/boot.js'
         ],
         'slice03.assets': [
             'slice03/static/src/**/*.js',
             'slice03/static/src/**/*.css',
+            ('remove', 'slice03/static/src/css/styles.css'),
+            ('remove', 'slice03/static/src/js/scripts.js')
         ]
     },
     'application': True,
